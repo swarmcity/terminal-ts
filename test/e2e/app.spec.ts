c52443945a598e68514ac06e934c17b15e2f9d41
Vojtech Simetka <vojtech@simetka.cz>
2022-04-21 20:14:00 +0500

    test: the complete happy path flow (#29)

--- test/e2e/app.spec.ts
+++ test/e2e/app.spec.ts
@@ -4,7 +4,7 @@ import type { PreviewServer } from 'vite'
 import puppeteer from 'puppeteer'
 import type { Browser, Page } from 'puppeteer'
 
-import { sleep } from '../utils'
+import { getProperty } from '../utils'
 
 const port = 3142 // Unlike in life, where there is no such thing as free PI, servers usually don't run on it
 const BASE_URL = `http://localhost:${port}`
@@ -34,11 +34,96 @@ describe("app.tsx", () => {
     const createAccountBtn = await page.waitForSelector('a[href="/create-account"]');
     await createAccountBtn.click()
 
-    await sleep(100) // just to be sure, sometimes it failed as the click handler is async
     const createAccountBtnText = await createAccountBtn.evaluate(el => el.textContent)
 
     expect(createAccountBtnText).toBe('Create account')
     const url = page.url()
     expect(url).toEqual(PAGES.createAccount)
   })
+
+  test("should display mnemonic in the create-account page", async () => {
+    await page.goto(PAGES.createAccount);
+    const wordsElement = await page.$$("li");
+    const words: string[] = []
+    for (let i = 0; i < wordsElement.length; i++)
+      words.push(await wordsElement[i].evaluate(el => el.textContent))
+
+    expect(words.length).toBe(12)
+  })
+
+  test("should go through the create-account flow", async () => {
+    await page.goto(PAGES.createAccount);
+    const wordsElement = await page.$$("li");
+    const words: string[] = []
+    for (let i = 0; i < wordsElement.length; i++)
+      words.push(await wordsElement[i].evaluate(el => el.textContent))
+    
+    expect(words.length).toBe(12)
+
+    let nextButton = await page.waitForSelector("button");
+    await nextButton.click()
+
+    // Validating the mnemonic words
+    let headerElement =  await page.waitForSelector("h1");
+    let headerText = await headerElement.evaluate(el => el.textContent)
+    expect(headerText).toBe('Fill in the correct words.')
+    const inputElements = await page.$$("input");
+    expect(inputElements.length).toBe(4)
+    for (let i = 0; i < inputElements.length; i++) {
+      const wordText = await getProperty(inputElements[i], 'placeholder')
+      const wordIndex = Number.parseInt(wordText.replace('Word ', ''))
+      await inputElements[i].focus()
+      await page.keyboard.type(words[wordIndex-1]) // input the words
+    }
+
+    nextButton = await page.waitForSelector("button");
+    await nextButton.click()
+
+    // Inputing in the username
+    headerElement =  await page.waitForSelector("h1");
+    headerText = await headerElement.evaluate(el => el.textContent)
+    expect(headerText).toBe('Choose a username and an avatar.')
+
+    const usernameElement = await page.waitForSelector("input");
+    await usernameElement.focus()
+    const username = 'testusername'
+    await page.keyboard.type('testusername')
+
+    nextButton = await page.waitForSelector("button");
+    await nextButton.click()
+
+    // Choose your password page with warning
+    headerElement =  await page.waitForSelector("h1");
+    headerText = await headerElement.evaluate(el => el.textContent)
+    expect(headerText).toBe('Choose a password.')
+
+    nextButton = await page.waitForSelector("button");
+    await nextButton.click()
+
+    // Choose your password page with two inputs
+    headerElement =  await page.waitForSelector("h1");
+    headerText = await headerElement.evaluate(el => el.textContent)
+    expect(headerText).toBe('Choose a password.')
+
+    const passwordInputElements = await page.$$("input");
+    expect(passwordInputElements.length).toBe(2)
+
+    const password = 'testpassword'
+    for (let i =0; i< passwordInputElements.length; i++) {
+      const el = passwordInputElements[0]
+      await el.focus()
+      await page.keyboard.type(password)
+    }
+
+    nextButton = await page.waitForSelector("button");
+    await nextButton.click()
+
+    // TODO: The encoding of the password takes ages, there must be better mechanism to test this
+    // await sleep(25000)
+
+    // // Final page
+    // headerElement =  await page.waitForSelector("h1");
+    // headerText = await headerElement.evaluate(el => el.textContent)
+    // expect(headerText).toBe('Great!')
+  }, 30000)
 });
--- test/utils.ts
+++ test/utils.ts
@@ -1,3 +1,9 @@
+import { ElementHandle } from "puppeteer";
+
 export async function sleep(ms: number): Promise<void> {
   return new Promise<void>(resolve => setTimeout(() => resolve(), ms))
+}
+
+export async function getProperty(element: ElementHandle, property: string): Promise<string> {
+    return await (await element.getProperty(property)).jsonValue();
 }
