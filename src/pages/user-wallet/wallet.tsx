2895cc2fd7c6a8c228c39659bbe32c95a7123755
Philippe Schommers <philippe@schommers.be>
2022-06-01 14:49:30 +0200

    chore: remove transaction view

--- src/pages/user-wallet/wallet.tsx
+++ src/pages/user-wallet/wallet.tsx
@@ -167,40 +167,6 @@ export const AccountWallet = (_: RouteComponentProps) => {
 			<div class="container">
 				<ViewComponent setView={setView} />
 			</div>
-			<div class="divider short" />
-			<div class="container">
-				<div class="flex-space">
-					<div class="history">
-						<div class="history-item">
-							<div class="date">13 Jan 2022 - 22:33</div>
-							<div class="tx-amt">Received 7 DAI from 0x0...</div>
-							<div>
-								<a href="#" class="link link-dark">
-									show on etherscan
-								</a>
-							</div>
-						</div>
-						<div class="history-item">
-							<div class="date">09 Jan 2022 - 22:33</div>
-							<div class="tx-amt">Sent 18 DAI to 0x0...</div>
-							<div>
-								<a href="#" class="link link-dark">
-									show on etherscan
-								</a>
-							</div>
-						</div>
-						<div class="history-item">
-							<div class="date">01 Dec 2021 - 19:03</div>
-							<div class="tx-amt">Received 7 DAI from 0x0...</div>
-							<div>
-								<a href="#" class="link link-dark">
-									show on etherscan
-								</a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
 		</div>
 	)
 }
